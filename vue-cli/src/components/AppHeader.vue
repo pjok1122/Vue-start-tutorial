63a8c968b860ff7858fc8329bb7b0f7ba6b42d13
Youngjae Park <pjok1122@naver.com>
2019-08-19 23:21:31 +0900

    Tutorial final

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <p>{{propsdata}}</p>
+      <button v-on:click="sendEvent">Send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods:{
+        sendEvent: function(){
+            this.$emit('renew');
+        }
+    }
+}
+    
+</script>
+
+<style>
+
+</style>
